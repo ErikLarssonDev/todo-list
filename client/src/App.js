469e6356ed4578698bbfdd2d9cf1f84f6d70fe42
ErikLarssonDev <86654747+ErikLarssonDev@users.noreply.github.com>
2022-06-05 21:44:42 +0200

    Client setup with create react app.

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import { Fragment } from 'react'
+
+// Components
+import InputTodo from './components/InputTodo';
+
+function App() {
+  return (
+   <Fragment>
+     <InputTodo />
+   </Fragment>
+  );
+}
+
+export default App;
---
+++ client/src/components/InputTodo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const InputTodo = () => {
+  return (
+    <div>
+      <h1>Input Todo</h1> 
+    </div>
+  )
+}
+
+export default InputTodo
