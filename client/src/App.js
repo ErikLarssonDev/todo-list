a79ec180d4e3072a5e22fd57e62b0448e3be1157
ErikLarssonDev <86654747+ErikLarssonDev@users.noreply.github.com>
2022-06-05 22:02:16 +0200

    Started on the input component.

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,20 @@
 import './App.css';
 import { Fragment } from 'react'
 
+// MUI imports
+import Container from '@mui/material/Container';
+
+
 // Components
 import InputTodo from './components/InputTodo';
 
 function App() {
   return (
-   <Fragment>
-     <InputTodo />
-   </Fragment>
+    <Fragment>
+      <Container sx={{ marginY: 5 }}>
+        <InputTodo />
+      </Container>
+    </Fragment>
   );
 }
 
--- client/src/components/InputTodo.js
+++ client/src/components/InputTodo.js
@@ -1,11 +1,32 @@
 import React from 'react'
+import { Fragment } from 'react'
+import { Box, Typography } from '@mui/material';
+import TextField from '@mui/material/TextField';
 
 const InputTodo = () => {
-  return (
-    <div>
-      <h1>Input Todo</h1> 
-    </div>
-  )
+    return (
+        <Box>
+            <Typography
+                variant='h4'
+                component='h1'
+                marginTop={5}
+                marginBottom={0}
+            >
+                Todo list
+            </Typography>
+            <Box
+                component="form"
+                sx={{
+                    '& > :not(style)': { m: 1, width: '25ch' },
+                }}
+                noValidate
+                autoComplete="off"
+            >
+                <TextField id="outlined-basic" label="Todo" variant="outlined" />
+            </Box>
+
+        </Box>
+    )
 }
 
 export default InputTodo
